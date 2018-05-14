5e0d9538536fb297046a000e17796d99767fc308
IanMa <mayueen@gmail.com>
2018-05-14 04:54:53 -0400

    Added course details and grades

---
+++ php/courses.php
@@ -0,0 +1,26 @@
+<?php
+
+$con = mysqli_connect('182.61.43.149',
+	'ianma',
+	'aen777',
+	'courses');
+
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"ajax_demo");
+$sql = "SELECT * FROM course_info NATURAL JOIN course_grades;";
+$result = mysqli_query($con,$sql);
+
+while($row = mysqli_fetch_array($result)) {
+	echo "<div class='one_course'>"
+    echo "<h2>" . $row['course_code'] . ":" . $row['course_name'] . "</h2>";
+    echo "<h3>Credit: " . $row['credit'] . ", My grade: " . $row['grade'] . "</h3>";
+    echo "<p>" . $row['description'] . "</p>";
+    echo "</div>"
+}
+
+mysqli_close($con);
+
+?>
